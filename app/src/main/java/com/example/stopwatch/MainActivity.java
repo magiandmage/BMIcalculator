8b785e4a47227003404deb34543a158c12f77590
yshibasaki <shibasaki.yutaro@gb-sense.com>
2020-02-19 17:30:42 +0900

    BMI計算

--- app/src/main/java/com/example/stopwatch/MainActivity.java
+++ app/src/main/java/com/example/stopwatch/MainActivity.java
@@ -6,11 +6,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Button execCalcBMI;      //計算開始ボタンへの参照
     private EditText hightnumber;   //身長入力欄への参照
     private EditText weightnumber;  //体重入力欄への参照
+    private TextView kekka;         //BMI結果への参照
 
 
     @Override
@@ -21,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         execCalcBMI = findViewById(R.id.button);  //ボタンの参照を取得
         hightnumber = findViewById(R.id.hightnumber); //身長入力欄の参照を取得
         weightnumber = findViewById(R.id.weightnumber); //体重入力欄の参照を取得
+        kekka = findViewById(R.id.kekka); //BMI結果表示欄の参照を取得
 
 
         execCalcBMI.setOnClickListener(new View.OnClickListener(){
@@ -29,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
                 double hightnumber_double = Double.valueOf(hightnumber.getText().toString());
                 double weightnumber_double = Double.valueOf(weightnumber.getText().toString());
                 double BMI = calcBMI(hightnumber_double, weightnumber_double);
+                String result = String.format("%f" , BMI);
+                kekka.setText(result);
             }
         });
     }
@@ -36,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
     private double calcBMI( double h, double w ){
         double bmi= 0;
         if(h > 0 && w > 0 ){
-            bmi =  w / (h * h);
+            bmi =  w / (h * h) * 10000;
         }
         return bmi;
     }
