79ec0fdb2f29940affb914c6af47bd9f68c42833
yshibasaki <shibasaki.yutaro@gb-sense.com>
2020-02-20 15:06:52 +0900

    BMI計算試作完成

--- app/src/main/java/com/example/BMIcalculator/MainActivity.java
+++ app/src/main/java/com/example/BMIcalculator/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText hightnumber;   //身長入力欄への参照
     private EditText weightnumber;  //体重入力欄への参照
     private TextView kekka;         //BMI結果への参照
+    private TextView hyouka;        //評価への参照
 
 
     @Override
@@ -24,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         hightnumber = findViewById(R.id.hightnumber); //身長入力欄の参照を取得
         weightnumber = findViewById(R.id.weightnumber); //体重入力欄の参照を取得
         kekka = findViewById(R.id.kekka); //BMI結果表示欄の参照を取得
+        hyouka = findViewById(R.id.hyouka);
 
 
         buttun.setOnClickListener(new View.OnClickListener(){
@@ -34,15 +36,33 @@ public class MainActivity extends AppCompatActivity {
                 double BMI = calcBMI(hightnumber_double, weightnumber_double);
                 String result = String.format("%.2f" , BMI);
                 kekka.setText(result);
+                String HYOUKA = "";
+                String hyoukabun = String.format("%s",HYOUKA );
+                hyouka.setText(hyoukabun);
             }
         });
     }
     //BMI算出
-    private double calcBMI( double h, double w ){
+     double calcBMI( double h, double w ){
         double bmi= 0;
         if(h > 0 && w > 0 ){
             bmi =  w / (h * h) * 10000;
         }
         return bmi;
+    }
+
+    //評価文分岐
+    void bun (double bmi){
+
+        if(bmi > 25.0){
+           System.out.print("でぶ");
+        }
+        else if(bmi < 18.5){
+            System.out.print("ほっそ");
+        }
+        else{
+            System.out.print("ふつー");
+        }
+
     }
 }
